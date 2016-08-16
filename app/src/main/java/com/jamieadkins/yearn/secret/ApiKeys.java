897f42e5340a1022c290cea1aa27b753486b6085
Jamie Adkins <jamieadkins95@gmail.com>
2016-08-16 22:44:48 +0100

    Added class that holds our secret information.
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

---
+++ app/src/main/java/com/jamieadkins/yearn/secret/ApiKeys.java
@@ -0,0 +1,9 @@
+package com.jamieadkins.yearn.secret;
+
+/**
+ * Class that holds API keys for the various services that we use.
+ */
+public class ApiKeys {
+    private static final String GOOGLE_PLACES_API_ANDROID = "Get your own!";
+    private static final String GOOGLE_PLACES_API_WEB = "Get your own!";
+}
