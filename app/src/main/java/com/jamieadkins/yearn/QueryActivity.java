1d5abfa85417b0988881c0b74b1bd26a958333e9
Jamie Adkins <jamieadkins95@gmail.com>
2016-09-17 23:08:35 +0100

    Import formatting
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/yearn/QueryActivity.java
+++ app/src/main/java/com/jamieadkins/yearn/QueryActivity.java
@@ -1,8 +1,9 @@
 package com.jamieadkins.yearn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import com.jamieadkins.yearn.ui.QueryFragment;
 
 public class QueryActivity extends AppCompatActivity {
