88cca0fa8ede1fbef7ed12bd3adad6342add3664
Jamie Adkins <jamieadkins95@gmail.com>
2016-11-01 12:54:28 +0000

    Toolbar is ow handled in BaseActivity
    Reduces same lines of code.
    
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/yearn/activities/QueryActivity.java
+++ app/src/main/java/com/jamieadkins/yearn/activities/QueryActivity.java
@@ -2,7 +2,6 @@ package com.jamieadkins.yearn.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -30,9 +29,6 @@ public class QueryActivity extends BaseActivity implements LocationProvider {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
