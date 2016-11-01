ed4b14faeaf23445591b7df58dec7741abcb351d
Jamie Adkins <jamieadkins95@gmail.com>
2016-11-01 10:35:01 +0000

    Added skeleton code for place picker
    Place picker can now be launched and the result is handled, just nothing in place to launch it yet.
    
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/yearn/activities/QueryActivity.java
+++ app/src/main/java/com/jamieadkins/yearn/activities/QueryActivity.java
@@ -1,12 +1,18 @@
 package com.jamieadkins.yearn.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.awareness.snapshot.LocationResult;
 import com.google.android.gms.awareness.snapshot.WeatherResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.maps.model.LatLng;
 import com.jamieadkins.yearn.R;
 import com.jamieadkins.yearn.ui.QueryFragment;
@@ -15,6 +21,9 @@ import com.jamieadkins.yearn.utils.WeatherUtils;
 
 public class QueryActivity extends BaseActivity implements LocationProvider {
     private final String TAG = getClass().getSimpleName();
+    private static final int PLACE_PICKER_CODE = 3294;
+    private Place mLocationForYearn;
+
     private ResultCallback<WeatherResult> mWeatherListener;
 
     @Override
@@ -29,6 +38,26 @@ public class QueryActivity extends BaseActivity implements LocationProvider {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_container, new QueryFragment())
                     .commit();
+        }
+    }
+
+    private void launchPlacePicker() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_CODE);
+        } catch (GooglePlayServicesRepairableException|GooglePlayServicesNotAvailableException e) {
+            Log.e(TAG, "Couldn't load place picker", e);
+            Toast.makeText(this, "Failed to load place picker", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_CODE) {
+            if (resultCode == RESULT_OK) {
+                mLocationForYearn = PlacePicker.getPlace(this, data);
+            }
         }
     }
 
@@ -60,7 +89,11 @@ public class QueryActivity extends BaseActivity implements LocationProvider {
 
     @Override
     public LatLng getLocationForYearn() {
-        // TODO Implement way for user to pick a location.
-        return null;
+        if (mLocationForYearn != null) {
+            return new LatLng(mLocationForYearn.getLatLng().latitude,
+                    mLocationForYearn.getLatLng().longitude);
+        } else {
+            return null;
+        }
     }
 }
