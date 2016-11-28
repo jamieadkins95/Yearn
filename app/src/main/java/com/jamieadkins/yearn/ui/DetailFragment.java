d36c98d93fd6cb26963a90064012e15b226c69f0
Jamie Adkins <jamieadkins95@gmail.com>
2016-11-28 13:07:46 +0000

    Detail fragment will now receive the place, ready to update the UI
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/yearn/ui/DetailFragment.java
+++ app/src/main/java/com/jamieadkins/yearn/ui/DetailFragment.java
@@ -1,17 +1,24 @@
 package com.jamieadkins.yearn.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
+import com.google.android.gms.location.places.Place;
 import com.jamieadkins.yearn.R;
+import com.jamieadkins.yearn.utils.OnPlaceFoundListener;
+import com.jamieadkins.yearn.utils.PlaceProvider;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailFragment extends Fragment {
+public class DetailFragment extends Fragment implements OnPlaceFoundListener {
+
+    private PlaceProvider mPlaceProvider;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,5 +26,23 @@ public class DetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mPlaceProvider = (PlaceProvider) context;
+        mPlaceProvider.setOnPlaceFoundListener(this);
+    }
+
+    @Override
+    public void onPlaceFound(Place place) {
+        // Update UI based on place.
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mPlaceProvider = null;
     }
 }
